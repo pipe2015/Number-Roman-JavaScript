ecd444d2d6b326c864b36df27093b96f9e83dcd0
pipe2015 <afelipe94@hotmail.com>
2018-10-27 21:58:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,68 +9,68 @@ var getFormatNumber = num => {
 
 var listFormat = (n, callback) => {
 	var n = n; 
-    var num = n;
+    	var num = n;
 	var range = 1; 
-    while(n != 0){
+    	while(n != 0){
 		var format = getFormatNumber(num);
-        n = format[format.length - 1];
-        if(!n){
+        	n = format[format.length - 1];
+        	if(!n){
 			if(!range){
 				break;
 			}
 			range = 0; 
         } 
         num = n;
-		callback.apply(this, format);
+	callback.apply(this, format);
     }
 }
 
 for(var i = 1; i <= 1000; i++){
 	var num = i;
-    var changeNum = num;
-    var entro = false;
+	var changeNum = num;
+	var entro = false;
 	var numLast = 0;
-    //var total = 1;
-    var numberCharRoman = "";
+	//var total = 1;
+	var numberCharRoman = "";
 	do {
 		var add = true;
-        var numeroRomano = "";
-        var valRomanCode = 0;
+        	var numeroRomano = "";
+        	var valRomanCode = 0;
 		//numeros 1 - 3
-        if(changeNum >= 1 && changeNum <= 3){
-            numeroRomano = 'I';
-            valRomanCode = 1;
-        }
+        	if(changeNum >= 1 && changeNum <= 3){
+            		numeroRomano = 'I';
+            		valRomanCode = 1;
+        	}
 		//numeros 4 - 8
-        if(changeNum >= 4 && changeNum <= 8){
-            numeroRomano = 'V';
-            valRomanCode = 5;
-        }
+        	if(changeNum >= 4 && changeNum <= 8){
+            		numeroRomano = 'V';
+            	valRomanCode = 5;
+        	}
 		//numeros entre 9 - 39
-        if(changeNum >= 9 && changeNum <= 39){
-            numeroRomano = 'X';
-            valRomanCode = 10;
-        }
+        	if(changeNum >= 9 && changeNum <= 39){
+            		numeroRomano = 'X';
+            		valRomanCode = 10;
+        	}
 		//numeros entre 40 - 89
-        if(changeNum >= 40 && changeNum <= 89){
-            numeroRomano = 'L';
-            valRomanCode = 50;
-        }
+        	if(changeNum >= 40 && changeNum <= 89){
+            		numeroRomano = 'L';
+            	valRomanCode = 50;
+        	}
 		//numeros entre 90 - 399
-        if(changeNum >= 90 && changeNum <= 399){
-            numeroRomano = 'C';
-            valRomanCode = 100;
-        }
+        	if(changeNum >= 90 && changeNum <= 399){
+            		numeroRomano = 'C';
+            		valRomanCode = 100;
+        	}
 		//numeros entre 400 - 900
-        if(changeNum >= 400 && changeNum <= 899){
-            numeroRomano = 'D';
-            valRomanCode = 500;
-        }
+        	if(changeNum >= 400 && changeNum <= 899){
+            		numeroRomano = 'D';
+            		valRomanCode = 500;
+        	}
 		//numeros entre 900 - 1000
 		if(changeNum >= 900 && changeNum <= 1000){
-            numeroRomano = 'M';
-            valRomanCode = 1000;
-        }
+	        	numeroRomano = 'M';
+            		valRomanCode = 1000;
+        	}
 		var res = Math.abs(changeNum - valRomanCode);
 		if((changeNum > 40 && changeNum < 50) || (changeNum > 90 && changeNum < 100) || (changeNum > 400 && changeNum < 500) || (changeNum > 900 && changeNum < 1000)){
 			if(changeNum > 100){
@@ -83,7 +83,7 @@ for(var i = 1; i <= 1000; i++){
 			add = false;
 		}
 		
-        changeNum = res;
+        	changeNum = res;
 		if(!changeNum){
 			changeNum = numLast;
 			if(numLast > 0){
@@ -93,6 +93,7 @@ for(var i = 1; i <= 1000; i++){
 		if(add){
 			numberCharRoman += numeroRomano;
 		}
+		
 	} while(changeNum != 0) {
 		var order = [
 			['VI', 4, 'IV'],
@@ -105,18 +106,18 @@ for(var i = 1; i <= 1000; i++){
 		var k = 0;
 		//ordenar los numeros
 		listFormat(i, (n1, n2) => {
-            order.forEach(e => {
-            	if(numberCharRoman.indexOf(e[0]) != -1){
-                	if(n1 == e[1] || n2 == e[1]){
-						if(n2 || !k){
-							numberCharRoman = numberCharRoman.replace(e[0], e[2]);	
-						}
-                   	}
-             	}
-            });
+            		order.forEach(e => {
+            		if(numberCharRoman.indexOf(e[0]) != -1){
+                		if(n1 == e[1] || n2 == e[1]){
+					if(n2 || !k){
+						numberCharRoman = numberCharRoman.replace(e[0], e[2]);	
+					}
+                   		}
+             		}
+            		});
 			k++;
 		});
 		
-    	console.log("Numero: ", i, " ", numberCharRoman); 	   
+    		console.log("Numero: ", i, " ", numberCharRoman); 	   
     }
 };
